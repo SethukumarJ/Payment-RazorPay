bbb0638776c7a2dcbc9f4d173891860e2307869b
Sethukumar J <sethukumarj.76@gmail.com>
2023-02-21 11:30:34 +0530

    passed data from backend side to template for success response

--- main.go
+++ main.go
@@ -2,54 +2,79 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	razorpay "github.com/razorpay/razorpay-go"
 )
 
 type PageVariables struct {
 	OrderId string
+	Email   string
+	Name    string
+	Amount  string
+	Contact string
 }
 
 func main() {
-	http.HandleFunc("/", App)
-	log.Fatal(http.ListenAndServe(":8089", nil))
+	router := gin.Default()
+	router.LoadHTMLGlob("*.html")
+	router.GET("/", App)
+	router.GET("/payment-success",PaymentSuccess)
+	router.Run(":8089")
 }
 
-func App(w http.ResponseWriter, r *http.Request) {
+func App(c *gin.Context) {
 
+	page := &PageVariables{}
+	page.Amount = "9000"
+	page.Email = "sethukumarj.76@gmail.com"
+	page.Name = "Sethukumar"
+	page.Contact = "7025493834"
 	//Create order_id from the server
 	client := razorpay.NewClient("rzp_test_kEtg65WKqGTpKd", "gPURxG4gzTmeNJKqqz61YCHV")
 
 	data := map[string]interface{}{
-		"amount":   50000,
+		"amount":   page.Amount,
 		"currency": "INR",
 		"receipt":  "some_receipt_id",
 	}
 	body, err := client.Order.Create(data, nil)
-
+	fmt.Println("////////////////reciept", body)
 	if err != nil {
 		fmt.Println("Problem getting the repository information", err)
 		os.Exit(1)
 	}
 
 	value := body["id"]
-	str := value.(string)
 
+	str := value.(string)
+	fmt.Println("str////////////////", str)
 	HomePageVars := PageVariables{ //store the order_id in a struct
 		OrderId: str,
+		Amount: page.Amount,
+		Email: page.Email,
+		Name: page.Name,
+		Contact: page.Contact,
 	}
 
-	t, err := template.ParseFiles("app.html")
-	if err != nil {
-		log.Print("template parsing error", err)
-	}
+	c.HTML(http.StatusOK, "app.html", HomePageVars)
+}
 
-	err = t.Execute(w, HomePageVars)
-	if err != nil {
-		log.Print("template excecuting error", err)
-	}
+
+func PaymentSuccess(c *gin.Context) {
+
+		paymentid := c.Query("paymentid")
+		orderid := c.Query("orderid")
+		signature := c.Query("signature")
+
+		fmt.Println(paymentid,"paymentid")
+		fmt.Println(orderid,"orderid")
+		fmt.Println(signature,"signature")
+}
+
+func PaymentFaliure(c *gin.Context) {
+
+
 }
